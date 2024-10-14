2dcb9e89e42d4fdb7e0f87d355cf2bfd1ab0addb
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-10-13 22:53:35 -0300

    projeto processo seletivo

---
+++ candidato/src/ProcessoSeletivo.java
@@ -0,0 +1,19 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2900.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
