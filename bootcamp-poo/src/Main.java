f8e9549235e38ab06cd2dd6363995eb9af4d1a60
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-10-19 13:35:14 -0300

    feat: add sign-up, progression and XP calculation tests to the Main class

--- bootcamp-poo/src/Main.java
+++ bootcamp-poo/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.bootcamp.dominio.Bootcamp;
 import br.com.dio.bootcamp.dominio.Curso;
+import br.com.dio.bootcamp.dominio.Dev;
 import br.com.dio.bootcamp.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,39 @@ public class Main {
         mJava.setDescricao("Descrição da mentoria Java");
         mJava.setData(LocalDate.now());
 
-        System.out.println(c1);
+        /*System.out.println(c1);
         System.out.println(c2);
-        System.out.println(mJava);
+        System.out.println(mJava);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(c1);
+        bootcamp.getConteudos().add(c2);
+        bootcamp.getConteudos().add(mJava);
+
+        Dev devMarcos = new Dev();
+        devMarcos.setNome("Marcos");
+        devMarcos.inscreverBootcamp(bootcamp);
+        System.out.println("=== Conteúdos Inscritos de " + devMarcos.getNome() + " ===");
+        System.out.println(devMarcos.getConteudosInscritos());
+
+        devMarcos.progredir();
+        System.out.println("Conteúdos Inscritos Restantes: " + devMarcos.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos: " + devMarcos.getConteudosConcluidos());
+        System.out.println("XP: " + devMarcos.calcularTotalXp());
+
+        Dev devDias = new Dev();
+        devDias.setNome("Dias");
+        devDias.inscreverBootcamp(bootcamp);
+        System.out.println("=== Conteúdos Inscritos de " + devDias.getNome() + " ===");
+        System.out.println(devDias.getConteudosInscritos());
+
+        devDias.progredir();
+        devDias.progredir();
+        devDias.progredir();
+        System.out.println("Conteúdos Inscritos Restantes: " + devDias.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos: " + devDias.getConteudosConcluidos());
+        System.out.println("XP: " + devDias.calcularTotalXp());
     }
 }
